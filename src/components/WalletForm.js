4d5cf1716aba0b558372f1fbfd4af91ce260f69d
Juliana Alvares <alvares.juliana@hotmail.com>
2022-07-29 16:49:12 -0300

    Requirement 3 - WalletForm component

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,9 +1,121 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { fetchCurrencyTypes } from '../redux/actions/walletActions';
 
-export default class WalletForm extends Component {
+class WalletForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      value: 0.00,
+      description: '',
+      currency: '',
+      method: '',
+      tag: '',
+    };
+  }
+
+  componentDidMount() {
+    const { fetchCurrencies } = this.props;
+    fetchCurrencies();
+  }
+
+  handleChange = ({ target }) => {
+    const { id, value } = target;
+    this.setState({
+      [id]: value,
+    });
+  }
+
   render() {
+    const { value, description, currency, method, tag } = this.state;
+    const { currencyOptions, loading } = this.props;
+    if (loading) return <span>Carregando...</span>;
     return (
-      <div>WalletForm</div>
+      <form>
+        <label htmlFor="value">
+          Valor:
+          <input
+            type="number"
+            data-testid="value-input"
+            id="value"
+            min={ 0 }
+            value={ value }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="currency">
+          Moeda:
+          <select
+            data-testid="currency-input"
+            id="currency"
+            value={ currency }
+            onChange={ this.handleChange }
+          >
+            {
+              (currencyOptions.length > 0)
+                && currencyOptions.map((element) => (
+                  <option key={ element }>{element}</option>
+                ))
+            }
+          </select>
+        </label>
+        <label htmlFor="method">
+          Método de pagamento:
+          <select
+            data-testid="method-input"
+            id="method"
+            value={ method }
+            onChange={ this.handleChange }
+          >
+            <option>Dinheiro</option>
+            <option>Cartão de crédito</option>
+            <option>Cartão de débito</option>
+          </select>
+        </label>
+        <label htmlFor="tag">
+          Categoria:
+          <select
+            data-testid="tag-input"
+            id="tag"
+            value={ tag }
+            onChange={ this.handleChange }
+          >
+            <option>Alimentação</option>
+            <option>Lazer</option>
+            <option>Trabalho</option>
+            <option>Transporte</option>
+            <option>Saúde</option>
+          </select>
+        </label>
+        <label htmlFor="description">
+          Descrição:
+          <input
+            type="text"
+            data-testid="description-input"
+            id="description"
+            value={ description }
+            onChange={ this.handleChange }
+          />
+        </label>
+      </form>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  currencyOptions: state.wallet.currencies,
+  loading: state.wallet.loading,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchCurrencies: () => dispatch(fetchCurrencyTypes()),
+});
+
+WalletForm.propTypes = {
+  currencyOptions: PropTypes.arrayOf(PropTypes.string).isRequired,
+  loading: PropTypes.bool.isRequired,
+  fetchCurrencies: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(WalletForm);
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,13 +1,27 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import {
+  REQUEST_CURRENCIES,
+  FAILED_REQUEST,
+  GET_CURRENCY_TYPES,
+} from '../actions/walletActions';
+
 const INITIAL_STATE = {
   currencies: [], // array de string
   expenses: [], // array de objetos, com cada objeto tendo as chaves id, value, currency, method, tag, description e exchangeRates
   editor: false, // valor booleano que indica se uma despesa está sendo editada
   idToEdit: 0,
+  requestError: '',
+  loading: false,
 };
 
 const wallet = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+  case REQUEST_CURRENCIES:
+    return { ...state, loading: true };
+  case GET_CURRENCY_TYPES:
+    return { ...state, loading: false, currencies: action.currencies };
+  case FAILED_REQUEST:
+    return { ...state, loading: false, requestError: action.requestError };
   default:
     return state;
   }
