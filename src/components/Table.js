249a1d04f3048c4f0e15770b1ee6bfcacc1a0b4b
Juliana Alvares <alvares.juliana@hotmail.com>
2022-08-01 11:19:09 -0300

    Requirement 6 - Table component formatting

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,7 +3,20 @@ import React, { Component } from 'react';
 export default class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <table className="expenses-table">
+        <caption>Lista de Despesas</caption>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+      </table>
     );
   }
 }
