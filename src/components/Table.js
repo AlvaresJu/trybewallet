ec7a6690e5e18c463d382e9a9a6746e85e7bfccc
Juliana Alvares <alvares.juliana@hotmail.com>
2022-08-01 12:14:38 -0300

    Requirement 7 - Logic to render Table rows with expenses

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,22 +1,62 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-export default class Table extends Component {
+class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <table className="expenses-table">
         <caption>Lista de Despesas</caption>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            (expenses.length > 0)
+              && expenses.map(({
+                id,
+                value,
+                description,
+                currency,
+                method,
+                tag,
+                exchangeRates,
+              }) => (
+                <tr key={ id }>
+                  <td>{description}</td>
+                  <td>{tag}</td>
+                  <td>{method}</td>
+                  <td>{Number(value).toFixed(2)}</td>
+                  <td>{exchangeRates[currency].name}</td>
+                  <td>{Number(exchangeRates[currency].ask).toFixed(2)}</td>
+                  <td>{Number(value * exchangeRates[currency].ask).toFixed(2)}</td>
+                  <td>Real</td>
+                </tr>
+              ))
+          }
+        </tbody>
       </table>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default connect(mapStateToProps, null)(Table);
