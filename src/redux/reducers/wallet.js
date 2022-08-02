a4d83913e0fcfc5cc02ce364464c6757fea2bbe0
Juliana Alvares <alvares.juliana@hotmail.com>
2022-08-01 22:31:23 -0300

    Change WalletForm locally controlled state to Redux State

--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,13 +1,12 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
-import {
-  REQUEST_DATA,
+import { REQUEST_DATA,
   FAILED_REQUEST,
   GET_CURRENCIES,
   ADD_EXPENSE,
   REMOVE_EXPENSE,
   EDIT_EXPENSE,
   SAVE_EDITED_EXPENSE,
-} from '../actions/walletActions';
+  CHANGE_INPUT } from '../actions/walletActions';
 
 const INITIAL_STATE = {
   currencies: [], // array de string
@@ -17,6 +16,13 @@ const INITIAL_STATE = {
   loading: false,
   requestError: '',
   totalExpense: 0,
+  formData: {
+    value: '',
+    description: '',
+    currency: 'USD',
+    method: 'Dinheiro',
+    tag: 'Alimentação',
+  },
 };
 
 const wallet = (state = INITIAL_STATE, action) => {
@@ -31,31 +37,36 @@ const wallet = (state = INITIAL_STATE, action) => {
     return { ...state,
       loading: false,
       expenses: [...state.expenses, action.expense],
-      totalExpense: (state.totalExpense + action.valueBRL) };
+      totalExpense: (state.totalExpense + action.valueBRL),
+      formData: { ...state.formData, value: '', description: '' } };
   case REMOVE_EXPENSE:
     return { ...state,
       expenses: state.expenses.filter(({ id }) => id !== action.idToRemove),
-      totalExpense:
-        (action.valueToRemove > state.totalExpense) ? 0
-          : (state.totalExpense - action.valueToRemove) };
+      totalExpense: (action.valueToRemove > state.totalExpense) ? 0
+        : (state.totalExpense - action.valueToRemove) };
   case EDIT_EXPENSE:
-    return { ...state, editor: true, idToEdit: action.idToEdit };
+    return { ...state,
+      editor: true,
+      idToEdit: action.idToEdit,
+      formData: { ...state.formData, ...action.inputsToEdit } };
   case SAVE_EDITED_EXPENSE:
     return { ...state,
       editor: false,
       expenses: state.expenses.map((expense) => {
         if (expense.id !== action.editedData.id) return expense;
-        return {
-          id: expense.id,
+        return { id: expense.id,
           value: action.editedData.value,
           description: action.editedData.description,
           currency: action.editedData.currency,
           method: action.editedData.method,
           tag: action.editedData.tag,
-          exchangeRates: expense.exchangeRates,
-        };
+          exchangeRates: expense.exchangeRates };
       }),
-      totalExpense: state.totalExpense - action.subValue + action.addValue };
+      totalExpense: (state.totalExpense - action.subValue + action.addValue),
+      formData: { ...state.formData, value: '', description: '' } };
+  case CHANGE_INPUT:
+    return { ...state,
+      formData: { ...state.formData, [action.input]: action.inputValue } };
   default:
     return state;
   }
